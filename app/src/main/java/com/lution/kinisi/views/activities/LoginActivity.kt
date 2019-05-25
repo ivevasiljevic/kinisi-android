d11ea4d878de11afdb4f7898473d9d12a4758e91
ivevasiljevic <ive@profico.hr>
2019-05-25 19:00:44 +0200

    added splash screen

--- app/src/main/java/com/lution/kinisi/views/activities/LoginActivity.kt
+++ app/src/main/java/com/lution/kinisi/views/activities/LoginActivity.kt
@@ -10,14 +10,9 @@ import kotlinx.android.synthetic.main.activity_login.*
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme_NoActionBar)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        //setSupportActionBar(toolbar)
     }
-
 }
