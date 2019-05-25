8332199422fa52b185d99ee400484ab91cd1e8d9
ivevasiljevic <ive@profico.hr>
2019-05-25 19:30:28 +0200

    added splash fragment

--- app/src/main/java/com/lution/kinisi/views/fragments/SplashFragment.kt
+++ app/src/main/java/com/lution/kinisi/views/fragments/SplashFragment.kt
@@ -1,10 +1,12 @@
 package com.lution.kinisi.views.fragments
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 import com.lution.kinisi.R
 
@@ -12,5 +14,15 @@ class SplashFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler().postDelayed({
+
+            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+
+        }, 200)
     }
 }
