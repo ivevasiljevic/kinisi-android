08cff5dba137694b4d5d45ab0a8eb4a874fd8b8d
ivevasiljevic <ive@profico.hr>
2019-05-26 20:34:18 +0200

    added basic register screen, added string and dimen values

--- app/src/main/java/com/lution/kinisi/views/fragments/RegisterFragment.kt
+++ app/src/main/java/com/lution/kinisi/views/fragments/RegisterFragment.kt
@@ -7,10 +7,25 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.lution.kinisi.R
+import com.lution.kinisi.adapters.ExposedDropdownMenuPopupItemAdapter
+import kotlinx.android.synthetic.main.fragment_register.*
 
 class RegisterFragment : Fragment() {
 
+    private val genders = arrayListOf("Male", "Female", "Other")
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setDropdownMenuPopupItemAdapter()
+    }
+
+    private fun setDropdownMenuPopupItemAdapter() {
+
+        exposedDropdownMenu.setAdapter(ExposedDropdownMenuPopupItemAdapter(context!!, R.layout.exposed_dropdown_menu_popup_item, genders))
     }
 }
