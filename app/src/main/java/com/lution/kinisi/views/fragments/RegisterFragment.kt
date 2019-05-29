a888c2f09d0e99b539912e952b3492bfd46c76f2
ivevasiljevic <ive@profico.hr>
2019-05-29 16:32:29 +0200

    changed register screen layout for better testing, added first usable version of validator utility

--- app/src/main/java/com/lution/kinisi/views/fragments/RegisterFragment.kt
+++ app/src/main/java/com/lution/kinisi/views/fragments/RegisterFragment.kt
@@ -8,11 +8,16 @@ import android.view.ViewGroup
 
 import com.lution.kinisi.R
 import com.lution.kinisi.adapters.ExposedDropdownMenuPopupItemAdapter
+import com.lution.kinisi.utils.BasicValidator
+import com.lution.kinisi.utils.EmailValidator
+import com.lution.kinisi.utils.PasswordValidator
+import com.lution.kinisi.utils.Validator
 import kotlinx.android.synthetic.main.fragment_register.*
 
 class RegisterFragment : Fragment() {
 
     private val genders = arrayListOf("Male", "Female", "Other")
+    private lateinit var validator: Validator
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_register, container, false)
@@ -21,11 +26,42 @@ class RegisterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        validator = Validator()
         setDropdownMenuPopupItemAdapter()
+
+        registerButton.setOnClickListener {
+
+            makeRegisterRequest()
+        }
     }
 
     private fun setDropdownMenuPopupItemAdapter() {
 
         exposedDropdownMenu.setAdapter(ExposedDropdownMenuPopupItemAdapter(context!!, R.layout.exposed_dropdown_menu_popup_item, genders))
+    }
+
+    private fun makeRegisterRequest() {
+
+        if(validateUserBasicInputs() && validateUserEmail() && validateUserPassword()) {
+
+            //do request to server
+        }
+    }
+
+    private fun validateUserEmail(): Boolean {
+
+        return validator.validate(editTextEmail, textInputEmail, arrayListOf(BasicValidator(), EmailValidator()))
+    }
+
+    private fun validateUserPassword(): Boolean {
+
+        return validator.validate(editTextPassword, textInputPassword, arrayListOf(BasicValidator(), PasswordValidator()))
+                && validator.validate(editTextRepeatPassword, textInputRepeatPassword, arrayListOf(BasicValidator(), PasswordValidator()))
+    }
+
+    private fun validateUserBasicInputs(): Boolean {
+
+        return validator.validate(editTextFirstname, textInputFirstname, arrayListOf(BasicValidator()))
+                && validator.validate(editTextLastname, textInputLastname, arrayListOf(BasicValidator()))
     }
 }
